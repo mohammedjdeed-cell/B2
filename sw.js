49b44649fd2373751687106ff6822fbf8a22e2a2
mohammedjdeed-cell <mohammedjdeed@gmail.com>
2026-06-17 01:12:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,36 @@
 const CACHE_NAME = 'b2-trainer-v1';
 const ASSETS = [
-  'index.html',
-  'topics.json'
+  './',
+  './index.html',
+  './topics.json',
+  './manifest.json'
 ];
 
+// Install event: Cache the vital static assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }).then(() => self.skipWaiting())
   );
 });
 
+// Activate event: Clean up any old cached files from previous versions
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch event: Attempt network request first; fall back to cache if offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => {
